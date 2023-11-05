9d1e0fea33cffae2bc5bfd7ee1fea0df3e5619ce
Alex Sergeev <sanhokk@yandex.ru>
2023-11-05 16:41:53 +0300

    New commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
-//import WallService.add
+import WallService.add
 import WallService.lastId
 import WallService.posts
-//import WallService.update
+import WallService.update
 
 data class Post(
     var id: Int = 0,
@@ -13,7 +13,8 @@ data class Post(
     val isPinned: Boolean = false,
     val date: Int,
     val friendsOnly: Boolean = false,
-    val postType: String = "post") {
+    val postType: String = "post",
+    val likes: Likes = Likes()) {
     class Likes(
         var count: Int = 0,
         var canLike: Boolean = true,
@@ -24,18 +25,16 @@ data class Post(
 object WallService {
     public var posts = emptyArray<Post>()
     public var lastId: Int = 0
-}
+
     fun add(post: Post): Post {
-        posts += post
-        lastId++
-        post.id = lastId
+        posts += post.copy(id = ++lastId)
         return posts.last()
     }
 
-    fun update(post: Post, id: Int):Boolean {
+    fun update(post: Post, id: Int): Boolean {
         for ((index, post) in posts.withIndex()) {
             if (id == post.id) {
-                posts[index] = post.copy(id = post.id + 1)
+                posts[index] = post.copy()
                 return true
             }
         }
@@ -43,19 +42,20 @@ object WallService {
     }
 
     fun clear() {
-        TODO("Not yet implemented")
+        lastId = 0
+        posts = emptyArray()
     }
 
-
+}
 
 fun main() {
 
     val new = Post(ownerId = 123, id = 1, text = "hello", date = 123456)
-    val old = Post(ownerId = 13, id = 1, text = "hllo", date = 12456)
+    val old = Post(ownerId = 13, id = 2, text = "hllo", date = 12456)
     println(add(new))
     println(add(old))
-    println(update(new, 2))
-    println(update(old, 2))
+    println(update(new, 1))
+    println(update(old, 45))
     println(new.id)
     println(old.id)
 }
--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -7,70 +7,41 @@ import org.junit.Test
 import org.junit.Assert.*
 import org.junit.Before
 
-data class Post(
-    var id: Int = 0,
-    val ownerId: Int,
-    val text: String,
-    val canPin: Boolean = true,
-    val canDelete: Boolean = true,
-    val canEdit:Boolean = true,
-    val isPinned: Boolean = false,
-    val date: Int,
-    val friendsOnly: Boolean = false,
-    val postType: String = "post") {
-    class Likes(
-        var count: Int = 0,
-        var canLike: Boolean = true,
-        var canPublish: Boolean = true,
-        var userLikes: Boolean = false
-    )
-}
-
 class WallServiceTest {
 
-//    @Before
-//    fun clearBeforeTest() {
-//        clear()
-//    }
+    @Before
+    fun clearBeforeTest() {
+        WallService.clear()
+    }
 
     @Test
     fun add() {
-        val post = Post(id = 0,
-        ownerId = 123,
-        text = "hello",
-        canPin = true,
-        canDelete = true,
-        canEdit = true,
-        isPinned = false,
-        date = 78965,
-        friendsOnly = false,
-        postType = "post")
-        val lastId = 0
+        val new = Post(ownerId = 123, id = 1, text = "hello", date = 123456)
 
-        val result = add(post)
+        val result = WallService.add(new)
 
-        assertEquals(Post(id=1, ownerId=123, text="hello", canPin=true, canDelete=true, canEdit=true, isPinned=false, date=78965, friendsOnly=false, postType="post"), result)
+        assertEquals(1, result.id)
 
     }
 
     @Test
     fun updateTrue() {
         val new = Post(ownerId = 123, id = 1, text = "hello", date = 123456)
-        posts+=new
+//        posts+=new
+        WallService.add(new)
+        val result = WallService.update(new, 1)
 
-        val result = update(new, 1)
-
-        assertEquals(true, result)
+        assertTrue(result)
     }
 
 
     @Test
     fun updateFalse() {
         val new = Post(ownerId = 123, id = 1, text = "hello", date = 123456)
-        posts+=new
+//        posts+=new
+        WallService.add(new)
+        val result = WallService.update(new, 2)
 
-        val result = update(new, 2)
-
-        assertEquals(false, result)
+        assertFalse(result)
     }
 }
